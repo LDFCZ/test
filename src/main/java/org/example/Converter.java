5f21e982365107542056f1c008401bb72367685d
LDFCZ <v.lopatkin1@g.nsu.ru>
2022-11-24 18:07:41 +0700

    fix adding binary files

--- src/main/java/org/example/Converter.java
+++ src/main/java/org/example/Converter.java
@@ -7,20 +7,20 @@ import java.util.*;
 public class Converter {
 
     // По идее было бы не плохо проверить на то, что некоторые ноды могут ссылаться друг на друга, в таком случае они не попадут
-    // в результирующий список. Эту проверку можно сделать с помощью трассировки, как в алгоритме GC.
+    // в результирующий список. Эту проверку можно сделать с помощью трассировки, как в алгоритме GC. Но в условиях задачи это не рассматривалось
+
+    // Если TreeEntity будут браться не из БД, то так же нужно проверять на парные id и на id != null. В противном случае
+    // одобные сущности нарушали бы условия целостности при хранении в БД.
     public Collection<TreeDTO> convert(Collection<TreeEntity> entities) {
+
+        if (entities == null) {
+            throw new RuntimeException("Null TreeEntity collection");
+        }
 
         Map<Integer, TreeDTO> idTreeDTOMap = new HashMap<>();
         List<TreeDTO> result = new ArrayList<>();
 
         entities.forEach(entity -> {
-            if (entity.getId() == null) {
-                throw new RuntimeException("Null id in TreeEntity with name - " + entity.getName());
-            }
-            if (idTreeDTOMap.containsKey(entity.getId())) {
-                throw new RuntimeException("There are more than one node with id - " + entity.getId());
-            }
-
             TreeDTO treeDTO = new TreeDTO(entity.getId(), entity.getName());
             idTreeDTOMap.put(entity.getId(), treeDTO);
 
--- src/test/java/org/example/ConverterTest.java
+++ src/test/java/org/example/ConverterTest.java
@@ -35,17 +35,86 @@ class ConverterTest {
         treeEntities.add(new TreeEntity(3, "3", 1));
         treeEntities.add(new TreeEntity(4, "4", 1));
 
-        Collection<TreeDTO> expectedCollection = new ArrayList<>();
+        Collection<TreeDTO> target = new ArrayList<>();
 
-        List<TreeDTO> expectedChildren = new ArrayList<>();
-        expectedChildren.add(new TreeDTO(2, "2"));
-        expectedChildren.add(new TreeDTO(3, "3"));
-        expectedChildren.add(new TreeDTO(4, "4"));
-        expectedCollection.add(new TreeDTO(1, "1", expectedChildren));
+        List<TreeDTO> children = new ArrayList<>();
+        children.add(new TreeDTO(2, "2"));
+        children.add(new TreeDTO(3, "3"));
+        children.add(new TreeDTO(4, "4"));
 
-        Collection<TreeDTO> answerCollection = converter.convert(treeEntities);
+        target.add(new TreeDTO(1, "1", children));
 
-        assertEquals(expectedCollection.toString(), answerCollection.toString());
+        Collection<TreeDTO> result = converter.convert(treeEntities);
+
+        assertEquals(target.toString(), result.toString());
+    }
+
+    private List<TreeEntity> createTestTree() {
+        List<TreeEntity> treeEntities = new ArrayList<>();
+        // First component
+        // Root
+        treeEntities.add(new TreeEntity(1, "1", null));
+        // Children
+        treeEntities.add(new TreeEntity(2, "2", 1));
+        treeEntities.add(new TreeEntity(3, "3", 1));
+        treeEntities.add(new TreeEntity(4, "4", 1));
+
+        // Second component
+        // Root
+        treeEntities.add(new TreeEntity(10, "10", null));
+        // Children
+        treeEntities.add(new TreeEntity(20, "20", 10));
+        treeEntities.add(new TreeEntity(30, "30", 10));
+        treeEntities.add(new TreeEntity(40, "40", 10));
+        // Children's children
+        treeEntities.add(new TreeEntity(50, "50", 20));
+        treeEntities.add(new TreeEntity(60, "60", 20));
+        treeEntities.add(new TreeEntity(70, "70", 40));
+        return treeEntities;
+    }
+
+    private  Collection<TreeDTO> createTargetTree() {
+        Collection<TreeDTO> target = new ArrayList<>();
+
+        // First component
+        // Children
+        List<TreeDTO> children1 = new ArrayList<>();
+        children1.add(new TreeDTO(2, "2"));
+        children1.add(new TreeDTO(3, "3"));
+        children1.add(new TreeDTO(4, "4"));
+        // Root
+        target.add(new TreeDTO(1, "1", children1));
+
+        // Second component
+        // Children's children
+        List<TreeDTO> children20 = new ArrayList<>();
+        children20.add(new TreeDTO(50, "50"));
+        children20.add(new TreeDTO(60, "60"));
+
+        List<TreeDTO> children40 = new ArrayList<>();
+        children40.add(new TreeDTO(70, "70"));
+        // Children
+        List<TreeDTO> children10 = new ArrayList<>();
+        children10.add(new TreeDTO(20, "20", children20));
+        children10.add(new TreeDTO(30, "30"));
+        children10.add(new TreeDTO(40, "40", children40));
+        // Root
+        target.add(new TreeDTO(10, "10", children10));
+
+        return target;
+    }
+
+    @Test
+    @DisplayName("Test with correct two-component tree")
+    public void twoComponentTreeTest() {
+
+        List<TreeEntity> treeEntities = this.createTestTree();
+
+        Collection<TreeDTO> target = this.createTargetTree();
+
+        Collection<TreeDTO> result = converter.convert(treeEntities);
+
+        assertEquals(target.toString(), result.toString());
     }
 
     public static final int NODE_COUNT = 1000;
@@ -61,7 +130,7 @@ class ConverterTest {
             treeEntities.add(new TreeEntity(i, String.valueOf(i), i - 1));
         }
 
-        Collection<TreeDTO> expectedCollection = new ArrayList<>();
+        Collection<TreeDTO> target = new ArrayList<>();
         TreeDTO previous = new TreeDTO(NODE_COUNT - 1, String.valueOf(NODE_COUNT - 1));
 
         for (int i = NODE_COUNT - 2; i >= 0; i--) {
@@ -70,11 +139,23 @@ class ConverterTest {
             previous = new TreeDTO(i, String.valueOf(i), children);
         }
 
-        expectedCollection.add(previous);
+        target.add(previous);
 
+        Collection<TreeDTO> result = converter.convert(treeEntities);
+        assertEquals(target.toString(), result.toString());
+    }
 
-        Collection<TreeDTO> answerCollection = converter.convert(treeEntities);
-        assertEquals(expectedCollection.toString(), answerCollection.toString());
+    @Test
+    @DisplayName("Empty input collection")
+    public void emptyInputCollectionTest() {
+
+        List<TreeEntity> treeEntities = new ArrayList<>();
+
+        Collection<TreeDTO> target = new ArrayList<>();
+
+        Collection<TreeDTO> result = converter.convert(treeEntities);
+
+        assertEquals(target.toString(), result.toString());
     }
 
 
@@ -97,39 +178,14 @@ class ConverterTest {
     }
 
     @Test
-    @DisplayName("Test exception on null id in entity")
+    @DisplayName("Test exception on null as input")
     public void nullIdExceptionTest() {
 
-        List<TreeEntity> treeEntities = new ArrayList<>();
-
-        treeEntities.add(new TreeEntity(1, "1", null));
-        treeEntities.add(new TreeEntity(2, "2", 1));
-        treeEntities.add(new TreeEntity(null, "3", 1));
-        treeEntities.add(new TreeEntity(4, "4", 1));
-
         RuntimeException thrown = assertThrows(RuntimeException.class, () -> {
-            converter.convert(treeEntities);
+            converter.convert(null);
         });
 
-        assertEquals("Null id in TreeEntity with name - 3", thrown.getMessage());
-    }
-
-    @Test
-    @DisplayName("Test exception on same ids in entities")
-    public void sameIdsExceptionTest() {
-
-        List<TreeEntity> treeEntities = new ArrayList<>();
-
-        treeEntities.add(new TreeEntity(1, "1", null));
-        treeEntities.add(new TreeEntity(2, "2", 1));
-        treeEntities.add(new TreeEntity(2, "3", 1));
-        treeEntities.add(new TreeEntity(4, "4", 1));
-
-        RuntimeException thrown = assertThrows(RuntimeException.class, () -> {
-            converter.convert(treeEntities);
-        });
-
-        assertEquals("There are more than one node with id - 2", thrown.getMessage());
+        assertEquals("Null TreeEntity collection", thrown.getMessage());
     }
 
 }
